4cc0cfa75e1c97512795d8bf0e93a053d916a4d1
sparklingLASER <mikubeautyrun@gmail.com>
2026-07-23 01:21:08 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,9 @@ export const metadata = {
     '元日本選手権出場トレーナーによるマンツーマンランニング指導。マラソン完走・ダイエット・ボディメイクに特化。皇居周り・隅田川周りで対面指導＆オンラインコーチングも対応。LINEで申し込み受付中。',
   keywords:
     '皇居ランニング,パーソナルトレーニング,ランニング指導,マラソン完走,ボディメイク,ダイエット,食事指導,東京ランニング,皇居,隅田川',
+  verification: {
+  google: '00x4-aSaw32Fukvr5L8fFL5e6VOehHFAfv-XAtmsBEQ',
+},
   openGraph: {
     title: 'LUKA! Running＆Workout | マラソン完走・ダイエット・ボディメイクに特化したパーソナルランニング指導',
     description:
