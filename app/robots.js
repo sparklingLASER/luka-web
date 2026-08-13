a00aec63b589e481e745344811c92ce6a9aecbaa
sparklingLASER <mikubeautyrun@gmail.com>
2026-08-13 15:28:17 +0000

    robots.js を更新

--- app/robots.js
+++ app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.lukarunninng.com/sitemap.xml',
+    sitemap: 'https://lukarunning.com/sitemap.xml',
   };
 }
